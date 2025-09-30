876970e8c76f03442b74fed056e3306eba115e81
jeffersonmendezgh <227472078+Jeffersonmendezgh@users.noreply.github.com>
2025-09-29 20:00:53 -0500

    Solucion a la desconeccion del WebScocket

--- backend-matrona/routers/__init__.py
+++ backend-matrona/routers/__init__.py
@@ -1,4 +1,5 @@
 from .usuario import router as usuario_router
 from .inventario import router as inventario_router
+from .ws_router import router as ws_router
 
-__all__ = ["usuario_router", "inventario_router"]
+__all__ = ["usuario_router", "inventario_router", "ws_router"]
