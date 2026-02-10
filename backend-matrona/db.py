1783a6e7c1bfbe933703f2153a62088218fe9221
Jefferson <jefferson18.estudios@gmail.com>
2026-02-10 06:43:01 -0500

    vinculacion db en Asus

--- backend-matrona/db.py
+++ backend-matrona/db.py
@@ -1,18 +1,19 @@
 # db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "#1004820393CC"
+MYSQL_PASSWORD = "#1004820393cc"
 MYSQL_HOST = "127.0.0.1"
 MYSQL_PORT = "3306"
 MYSQL_DB = "proyecto_matrona"
 
 DATABASE_URL = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4"
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@"f"{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4"
 )
 
-engine = create_engine(DATABASE_URL, pool_recycle=3600)
+engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base() #clase base para todos los modelos sqlalchemy
